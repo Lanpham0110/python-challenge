1446ae8d1d03266e6085e3a29f374d4c80b8dc37
Lan Pham <Lanpham0110@gmail.com>
2020-10-02 03:42:32 -0600

    gworking on average change

--- PyBank/Analysis/main.py
+++ PyBank/Analysis/main.py
@@ -1,9 +1,8 @@
 import os
 import csv
-import statistics
 
 #create a path
-csvpath=os.path.join("Resources","budget_data.csv")
+csvpath=os.path.join("..","Resources","budget_data.csv")
 #open the csv
 #read that csv file
 with open (csvpath,newline='') as csvfile:
@@ -12,19 +11,21 @@ with open (csvpath,newline='') as csvfile:
     csv_header=next(csvreader,None)
 #do a for loop to run thru each row: index [0] for month, index [1] for amount of profit/losses
 #create empty list so when going thru row, adding each count into list
-    countMonth=[]
-    Net_total_profit_losses=[]
+    countMonth=0
     Net_total=0
     average=[]
-    for row in csvreader:
-        countMonth.append(row[0])
-        print(f'Total Month: {len(countMonth)}')
-        Net_total=Net_total +int(row[1])
-        Net_total_profit_losses.append(Net_total)
+    for i in csvreader:
+        countMonth+=1
+        print(f'Total Month:{countMonth}')
+        Net_total=Net_total +int(i[1])
         print(f'Total: {Net_total}')
-        average_change = Net_total/(len(Net_total_profit_losses))
-        #aversge= statistics.mean(row[1])
-        print(f'Average Change: {average_change}')
+       
+        for j in csvreader:
+        
+        
+            average_change =(int(j[1])-int(i[1])) /((int(j[0])-int(i[0]))
+     
+            print(f'Average Change: {average_change}')
 
     
     
