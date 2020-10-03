de58813540a23b92963d57304534a5179455f411
Lan Pham <Lanpham0110@gmail.com>
2020-10-03 14:26:40 -0600

    created output text file

--- PyPoll/Analysis/main.py
+++ PyPoll/Analysis/main.py
@@ -12,13 +12,13 @@ with open (csvpath,newline='') as csvfile:
     csv_header=next(csvreader,None)
   
     Vote_count=0
-    count_name=[]
+    Totalcount_name=[]
     Candidate_names=[]
     name=[]
     vote_list=[]
     i=0
-    sumchange=0
-    change_list=[]
+    percentage=[]
+    
     for row in csvreader:
         #print(row)
         Vote_count+=1
@@ -30,24 +30,25 @@ with open (csvpath,newline='') as csvfile:
         counting_name=name.count(Candidate_names[i])
         if counting_name ==0:
             name.append(Candidate_names[i])
-    print(name)
-      #  count_name+=1
+            #look into name list, if i name is not in list, add that name from big list into name list
+            #identifying names of candidates 
 
-    
     for j in range(0,len(name)):     
             
-        count_name.append(Candidate_names.count(name[j]))
+        Totalcount_name.append(Candidate_names.count(name[j]))
+        #inside name list, calculate total votes of each name in that list
         
-        
-        #sumchange=sumchange+round(change)
-        #print(change)
-        #change_list.append(change)
-    #Greatest_value=max(winner)
-   
-    #average_change =sumchange/len(change_list)   
-    print(f'Total Votes:{Vote_count}')
-    print(f'Total name: {count_name}')   
-    #print(f'Average Change: {average_change}')
-    #print(f'Greatest value = {Greatest_value}')
-    #print(f'Lowest value = {Lowest_value}')
+        percentage.append(round((Totalcount_name[j]/Vote_count)*100))
+
+    with open ("output.txt","w") as outfile:
+        print(f'Total Votes:{Vote_count}')
+        outfile.write(f'Total Votes:{Vote_count}\n')
+     #Greatest_value=max(winner) 
+         
+     
+        for j in range(0,len(name)):    
+            print(f"{name[j]} {Totalcount_name[j]} {percentage[j]}")
+            outfile.write(f"{name[j]} {percentage[j]}% ({Totalcount_name[j]})\n")
+
+    
     
