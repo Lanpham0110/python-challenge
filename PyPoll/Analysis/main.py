0e9b56fbcd18772a0609c1d8f20b0328ac3e67f7
Lan Pham <Lanpham0110@gmail.com>
2020-10-03 00:33:42 -0600

    working on PyPoll

--- PyPoll/Analysis/main.py
+++ PyPoll/Analysis/main.py
@@ -24,28 +24,29 @@ with open (csvpath,newline='') as csvfile:
         Vote_count+=1
         Candidate_names.append(row[2])
         # add all values in 3rd row index 2 into cadidate name list
-        
-    #for i in range(len(Candidate_names))
-      #  count_name=name.count[i]
-      #  count_name+=1
-
-    for i in range(0,(len(Candidate_names)-1)):
-        if (Candidate_names[i])!=(Candidate_names[i+1]):
+    
+    name.append(Candidate_names[0])
+    for i in range(1,len(Candidate_names)):
+        counting_name=name.count(Candidate_names[i])
+        if counting_name ==0:
             name.append(Candidate_names[i])
     print(name)
-   # for j in range(0,len(name)):     
+      #  count_name+=1
+
+    
+    for j in range(0,len(name)):     
             
-       # count_name.append(Candidate_names.count(name[j]))
+        count_name.append(Candidate_names.count(name[j]))
         
         
         #sumchange=sumchange+round(change)
         #print(change)
         #change_list.append(change)
-    #Greatest_value=max(change_list)
-    #Lowest_value=min(change_list)
+    #Greatest_value=max(winner)
+   
     #average_change =sumchange/len(change_list)   
     print(f'Total Votes:{Vote_count}')
-    #print(f'Total name: {count_name}')   
+    print(f'Total name: {count_name}')   
     #print(f'Average Change: {average_change}')
     #print(f'Greatest value = {Greatest_value}')
     #print(f'Lowest value = {Lowest_value}')
