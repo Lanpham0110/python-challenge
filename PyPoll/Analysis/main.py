4c4cf6944367d99b17fad0b47aa72a1b6d035cc7
Lan Pham <Lanpham0110@gmail.com>
2020-10-03 20:50:00 -0600

    created dictionary

--- PyPoll/Analysis/main.py
+++ PyPoll/Analysis/main.py
@@ -18,7 +18,7 @@ with open (csvpath,newline='') as csvfile:
     vote_list=[]
     i=0
     percentage=[]
-    
+    dict={}
     for row in csvreader:
         #print(row)
         Vote_count+=1
@@ -26,29 +26,38 @@ with open (csvpath,newline='') as csvfile:
         # add all values in 3rd row index 2 into cadidate name list
     
     name.append(Candidate_names[0])
+    dict[Candidate_names[0]]=[] 
     for i in range(1,len(Candidate_names)):
         counting_name=name.count(Candidate_names[i])
         if counting_name ==0:
             name.append(Candidate_names[i])
             #look into name list, if i name is not in list, add that name from big list into name list
             #identifying names of candidates 
-
+            dict[Candidate_names[i]]=[]  
+            
     for j in range(0,len(name)):     
             
         Totalcount_name.append(Candidate_names.count(name[j]))
-        #inside name list, calculate total votes of each name in that list
+        #inside name list, calculate total votes of each Candidate in that list
+        dict[name[j]]=Totalcount_name[j]
         
         percentage.append(round((Totalcount_name[j]/Vote_count)*100))
 
     with open ("output.txt","w") as outfile:
         print(f'Total Votes:{Vote_count}')
         outfile.write(f'Total Votes:{Vote_count}\n')
-     #Greatest_value=max(winner) 
+     
          
      
         for j in range(0,len(name)):    
             print(f"{name[j]} {Totalcount_name[j]} {percentage[j]}")
             outfile.write(f"{name[j]} {percentage[j]}% ({Totalcount_name[j]})\n")
-
+            #print(f'Winner: {winner}')
+            #outfile.write(f'Winner: {winner}')
+            
+        winner=max(dict,key=dict.get)
+       #find max vote associate with its key in dictionary
+        print(winner)
+        outfile.write(f'Winner: {winner}')
     
     
